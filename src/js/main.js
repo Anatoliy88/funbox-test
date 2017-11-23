d2714f3efd9719b60899599dc355725a718b84a4
Anatoly <anatolyhaulin@gmail.com>
2017-11-24 02:23:03 +0300

    Стили и скрипты для состояния карточек товара

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,53 @@
 // @include('bowser.js')
 // @include('share.js')
 
+let $catalogItem = $('.catalog__item');
+let $itemWrapper = $('.item__wrapper');
+let $itemFooterLink = $('.item__footer-link a');
+
+$(document).ready(function() {
+	$catalogItem.each(function() {
+		if ($(this).hasClass('disabled')) {
+			let $itemTaste = $(this).find('.item__taste').text();
+
+			$('.item__footer-disabled').text(`Печалька, ${$itemTaste} закончился.`);
+		}
+	});
+});
+
+$itemFooterLink.on('click', function () {
+	$(this).parent().parent().parent().toggleClass('active');
+});
+
+$itemWrapper.on('click', function () {
+	if ($(this).hasClass('disabled') === false) {
+		$(this).parent().toggleClass('active');
+	}
+	if ($(this).parent().hasClass('hover') === true) {
+		$(this).parent().removeClass('hover');
+	}
+});
+
+$itemWrapper.on('mouseenter', function () {
+	if ($(this).parent().hasClass('disabled') === false) {
+		if ($(this).parent().hasClass('active') === true) {
+			$(this).parent().addClass('hover');
+		} else {
+			$(this).parent().removeClass('hover');
+		}
+	}
+});
+
+$itemWrapper.on('mouseleave', function () {
+	if ($(this).parent().hasClass('disabled') === false) {
+		if ($(this).parent().hasClass('active') === true) {
+			$(this).parent().removeClass('hover');
+		} else {
+			$(this).parent().removeClass('hover');
+		}
+	}
+});
+
 svg4everybody();
 
 let $html = $(document.documentElement);
